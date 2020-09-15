29547b077c4f209f8b2a03ac5e987319983be9bb
xiaobai22 <helloyourself@gmail.com>
2020-09-15 16:57:38 +0800

    init

--- api/main.go
+++ api/main.go
@@ -1,9 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"github.com/rz1226/blackboardkit"
+	"github.com/rz1226/fasthttpserv"
+	"github.com/valyala/fasthttp"
+)
 
-func main(){
+//web服务器， 对fasthttp的封装
+const (
+	RETURN_OK = `{"code":0,"msg":"ok"}`
 
+	RETURN_NOT_VALID_JSON = `{"code":-1,"msg":"not valid json error"}`
+	RETURN_ERR            = `{"code":-1,"msg":"err"}`
+)
 
+func Index(ctx *fasthttp.RequestCtx) string {
 
+	return `{"hello world"}`
 
+}
+
+func StartServ(port string) {
+	blackboardkit.StartMonitor("9090")
+
+	serv := fasthttpserv.NewServ()
+	serv.GET("/", Index)
+
+	fmt.Println("ready to start")
+	serv.StartCORS(port)
 }
